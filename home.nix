ba66bdcd2be9a9ae1c23ef7ff7aae39229a979ff
theabm <72303015+theabm@users.noreply.github.com>
2024-02-12 17:45:24 +0100

    updated system - removed neorg due to breaking changes

--- home.nix
+++ home.nix
@@ -303,27 +303,27 @@
       indent-blankline.enable = true;
       markdown-preview.enable = true;
       nvim-autopairs.enable = true;
-      neorg = {
-        enable = true;
-        modules = {
-          "core.defaults".__empty = null;
-          "core.concealer" = {
-            config = {
-              icon_preset = "diamond";
-            };
-          };
-          "core.dirman" = {
-            config = {
-              workspaces = {
-                notes = "~/notes";
-                on_the_way_to_10k = "~/on_the_way_to_10k";
-              };
-              default_workspace = "on_the_way_to_10k";
-            };
-          };
-          "core.summary".__empty = null;
-        };
-      };
+      # neorg = {
+      #   enable = true;
+      #   modules = {
+      #      "core.defaults".__empty = null;
+      #          "core.concealer" = {
+      #            config = {
+      #              icon_preset = "diamond";
+      #            };
+      #          };
+      #          "core.dirman" = {
+      #            config = {
+      #              workspaces = {
+      #                notes = "~/notes";
+      #                on_the_way_to_10k = "~/on_the_way_to_10k";
+      #              };
+      #              default_workspace = "on_the_way_to_10k";
+      #            };
+      #          };
+      #          "core.summary".__empty = null;
+      #   };
+      # };
 
       telescope = {
         enable = true;
