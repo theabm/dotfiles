9c3afb7a15383948accca50a9e5bf0b6f515bfa0
theabm <72303015+theabm@users.noreply.github.com>
2024-03-13 10:16:45 +0100

    moved to waybar folder

--- home/default.nix
+++ home/default.nix
@@ -26,7 +26,7 @@
     ./fish.nix
     ./himalaya.nix
     ./kitty.nix
-    ./waybar.nix
+    ./waybar
   ];
   # technically I dont need this since nixvim should take care of this.
   programs.ruff = {
