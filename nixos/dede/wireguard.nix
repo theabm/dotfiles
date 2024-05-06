5d8a4123b7ffe97cf58b9785b2dedc5289464cfe
theabm <72303015+theabm@users.noreply.github.com>
2024-05-06 23:58:54 +0200

    changed to 10.10.20.0/24

--- nixos/dede/wireguard.nix
+++ nixos/dede/wireguard.nix
@@ -15,7 +15,7 @@
 
     wireguard.interfaces.wg0 = {
       # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = ["10.10.10.2/24"];
+      ips = ["10.10.20.2/24"];
 
       # The port that WireGuard listens to. Must be accessible by the client.
       listenPort = port;
@@ -26,7 +26,7 @@
         {
           name = "server";
           publicKey = "4cxMtehccXYPILDgrvVc+/neazpgY361Z7fsURDjxHQ=";
-          allowedIPs = ["10.10.10.1/32"];
+          allowedIPs = ["10.10.20.1/32"];
           endpoint = "95.246.218.120:51820";
           persistentKeepalive = 25;
         }
--- nixos/server/wireguard.nix
+++ nixos/server/wireguard.nix
@@ -15,7 +15,7 @@
 
     wireguard.interfaces.wg0 = {
       # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = ["10.10.10.1/24"];
+      ips = ["10.10.20.1/24"];
 
       # The port that WireGuard listens to. Must be accessible by the client.
       listenPort = port;
@@ -26,12 +26,12 @@
         {
           name = "dede-laptop";
           publicKey = "ZFhTIwcZmjMjJaYw2nLx4Q3baHeW33F+UGuu5yfzWBI=";
-          allowedIPs = ["10.10.10.2/32"];
+          allowedIPs = ["10.10.20.2/32"];
         }
         {
           name = "phone";
           publicKey = "4mCjfoKI4xD+sMp8Li8gM31grhVBnrRnIsZCJrNduBc=";
-          allowedIPs = ["10.10.10.3/32"];
+          allowedIPs = ["10.10.20.3/32"];
         }
       ];
     };
