1819c7be068c60e6606e4ae38e26f17057465152
theabm <andres.bermeo-marinelli@inria.fr>
2024-09-11 13:49:37 +0200

    removed comments

--- nixos/dede/configuration.nix
+++ nixos/dede/configuration.nix
@@ -47,10 +47,6 @@ in {
   boot.initrd.luks.devices."luks-899e1d08-30b0-422d-a760-389d75acadf2".device = "/dev/disk/by-uuid/899e1d08-30b0-422d-a760-389d75acadf2";
   networking.hostName = "dede"; # Define your hostname.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager = {
     enable = true;
@@ -100,7 +96,6 @@ in {
   console.keyMap = "it2";
 
   # Enable sound with pipewire.
-  # sound.enable = false;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -153,7 +148,6 @@ in {
     zellij
     bat
     ripgrep
-    gcc
     zathura
     feh
     sddm_theme
@@ -175,10 +169,6 @@ in {
       enable = true;
     };
   };
-
-  # programs.steam = {
-  #   enable = true;
-  # };
 
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
@@ -231,31 +221,6 @@ in {
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
   nix = {
--- nixos/inria/configuration.nix
+++ nixos/inria/configuration.nix
@@ -19,11 +19,6 @@ in {
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "inria"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -45,10 +40,6 @@ in {
     LC_TELEPHONE = "it_IT.UTF-8";
     LC_TIME = "it_IT.UTF-8";
   };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
@@ -74,12 +65,6 @@ in {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   hardware = {
@@ -89,8 +74,6 @@ in {
     bluetooth.powerOnBoot = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.andres = {
@@ -99,7 +82,6 @@ in {
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
       kdePackages.kate
-      #  thunderbird
     ];
   };
 
@@ -114,9 +96,6 @@ in {
     openconnect
     wget
     kitty
-    # mako
-    # swww
-    # rofi-wayland
     git
     networkmanagerapplet
     lshw
@@ -125,7 +104,6 @@ in {
     zellij
     bat
     ripgrep
-    gcc
     zathura
     feh
     zotero
@@ -140,36 +118,13 @@ in {
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
 
+  virtualisation.docker.enable = true;
+
   fonts.packages = with pkgs; [
     (nerdfonts.override {fonts = ["Hack"];})
     font-awesome
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
   nix = {
     settings = {
