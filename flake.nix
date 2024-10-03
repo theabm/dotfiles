660f6043cbcabacb777eea770936ee5d9a47f820
theabm <72303015+theabm@users.noreply.github.com>
2024-10-03 09:59:41 +0200

    removed neorg overlay - added agenix rekey

--- flake.nix
+++ flake.nix
@@ -14,13 +14,18 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    neorg-overlay = {
-      url = "github:nvim-neorg/nixpkgs-neorg-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # neorg-overlay = {
+    #   url = "github:nvim-neorg/nixpkgs-neorg-overlay";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     agenix = {
       url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    agenix-rekey = {
+      url = "github:oddlama/agenix-rekey";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -30,13 +35,19 @@
     nixpkgs,
     home-manager,
     nixvim,
-    neorg-overlay,
+    # neorg-overlay,
     agenix,
+    agenix-rekey,
     ...
   } @ inputs: let
     system = "x86_64-linux";
   in {
     formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+
+    agenix-rekey = agenix-rekey.configure {
+      userFlake = self;
+      nodes = self.nixosConfigurations;
+    };
 
     nixosConfigurations = {
       dede = nixpkgs.lib.nixosSystem {
@@ -52,7 +63,7 @@
           home-manager.nixosModules.home-manager
 
           {
-            nixpkgs.overlays = [neorg-overlay.overlays.default];
+            # nixpkgs.overlays = [neorg-overlay.overlays.default];
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
@@ -78,7 +89,7 @@
           home-manager.nixosModules.home-manager
 
           {
-            nixpkgs.overlays = [neorg-overlay.overlays.default];
+            # nixpkgs.overlays = [neorg-overlay.overlays.default];
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
