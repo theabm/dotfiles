ddcb44fd50fa05edfc2c2588e70f345da618a948
theabm <72303015+theabm@users.noreply.github.com>
2024-10-07 10:20:07 +0200

    agenix

--- nixos/common/agenix.nix
+++ nixos/common/agenix.nix
@@ -9,12 +9,15 @@
   ];
 
   age.rekey = {
+    # store everything locally
     storageMode = "local";
+    # my master identity - used to decrypt secrets in repository.
     masterIdentities = [../../identity.age];
     localStorageDir = ../../secrets/${config.networking.hostName};
   };
 
   # enable ssh server for agenix configuration.
+  # This is the public-private key pair I will use to decrypt secrets at build time.
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
