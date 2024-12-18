a3246bbbe57e436d9435995219d6cf7049625d14
theabm <72303015+theabm@users.noreply.github.com>
2024-12-18 12:13:51 +0100

    added cachix

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -98,6 +98,12 @@ in {
       max-jobs = "auto";
       auto-optimise-store = true;
       trusted-users = ["root" "andres"];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     gc = {
       automatic = true;
