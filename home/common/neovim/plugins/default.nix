4b0aff36401e9a945afb3b6e80b7d264c6eb6545
theabm <72303015+theabm@users.noreply.github.com>
2025-02-06 00:13:51 +0100

    better comment

--- home/common/neovim/plugins/default.nix
+++ home/common/neovim/plugins/default.nix
@@ -10,24 +10,38 @@
   ];
 
   programs.nixvim.plugins = {
+    # indentation line guide
     indent-blankline.enable = true;
+    # preview markdown from nvim
     markdown-preview.enable = true;
+    # automatically create pairs () {} ..
     nvim-autopairs.enable = true;
+    # provide nerd icons for plugins
     web-devicons.enable = true;
 
+    # show color for color codes #FFFF00
     colorizer.enable = true;
 
+    # show context (for example, for long functions, it shows which function you 
+    # are inspecting)
     treesitter-context = {
       enable = true;
       settings.enable = true;
     };
 
+    # luasnippets
     luasnip.enable = true;
+    # commenting with ease
     comment.enable = true;
+    # git signs for viewing changes 
     gitsigns.enable = true;
+    # lua status line at the bottom 
     lualine.enable = true;
+    # show general structure of file (functions, classes, etc) in a side menu :TagbarToggle
     tagbar.enable = true;
+    # get to where you want to edit in a few keystrokes
     leap.enable = true;
+    # edit file directory like a normal vim buffer
     oil = {
       enable = true;
       settings = {
@@ -40,6 +54,7 @@
         };
       };
     };
+    # quickly surround 
     vim-surround.enable = true;
   };
 }
