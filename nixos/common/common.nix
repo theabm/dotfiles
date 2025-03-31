a9d93cd53aa43d270205f44557071300bd517367
theabm <andres.bermeo-marinelli@inria.fr>
2025-03-31 13:15:40 +0200

    spacing

--- nixos/common/common.nix
+++ nixos/common/common.nix
@@ -49,7 +49,7 @@ in {
   services.openssh = {
     enable = true;
     settings = {
-      PasswordAuthentication=false;
+      PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
     };
   };
