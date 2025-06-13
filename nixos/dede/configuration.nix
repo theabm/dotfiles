24fe077267f9c009690d2177ab1adcec01db3dd1
theabm <72303015+theabm@users.noreply.github.com>
2025-06-13 10:12:08 +0200

    add vscode

--- nixos/dede/configuration.nix
+++ nixos/dede/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
@@ -86,6 +83,7 @@ in {
     ollama-cuda
     ardour
     ente-auth
+    vscode
   ];
 
   # used for ardour
